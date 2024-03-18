9f0db8e496995d8ce876dc262584453c54ecbc1c
JoaoKremerDev <joaokremer.dev@gmail.com>
2024-03-18 10:29:36 -0300

    feat: Product model has been modified

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -7,13 +7,14 @@ const data: Product[] = [
     { title: 'Produto X', price: 10 },
     { title: 'Produto Y', price: 15 },
     { title: 'Produto Z', price: 20 },
+    { title: 'Produto T', price: 22 },
 ];
 
 export const Product = {
     getAll: (): Product[] => {
         return data;
     },
-    getPricecAfter: () => {
-        
+    getFromPricecAfter: (price: number): Product[] => {
+        return data.filter((item) => item.price >= price )
     }
 }
